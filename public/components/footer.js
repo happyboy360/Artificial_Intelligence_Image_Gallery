8c97f7c477c5ac1c0ab162aff9d9900be41ea714
Adam Griebel <adgriebe@esu10.org>
2023-04-10 14:33:24 -0500

    Got the header and footer components to work!

--- public/components/footer.js
+++ public/components/footer.js
@@ -5,55 +5,19 @@
 
     connectedCallback() {
         this.innerHTML = `
-      <style>
-        footer {
-          height: 60px;
-          padding: 0 10px;
-          list-style: none;
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          background-color: #dfdfe2;
-        }
-        
-        ul li {
-          list-style: none;
-          display: inline;
-        }
-        
-        a {
-          margin: 0 15px;
-          color: inherit;
-          text-decoration: none;
-        }
-        
-        a:hover {
-          padding-bottom: 5px;
-          box-shadow: inset 0 -2px 0 0 #333;
-        }
-        
-        .social-row {
-          font-size: 20px;
-        }
-        
-        .social-row li a {
-          margin: 0 15px;
-        }
-      </style>
-      <footer>
-        <ul>
-          <li><a href="about.html">About</a></li>
-          <li><a href="work.html">Work</a></li>
-          <li><a href="contact.html">Contact</a></li>
-        </ul>
-        <ul class="social-row">
-          <li><a href="https://github.com/my-github-profile"><i class="fab fa-github"></i></a></li>
-          <li><a href="https://twitter.com/my-twitter-profile"><i class="fab fa-twitter"></i></a></li>
-          <li><a href="https://www.linkedin.com/in/my-linkedin-profile"><i class="fab fa-linkedin"></i></a></li>
-        </ul>
-      </footer>
-    `;
+     <footer class="section-relative section-34 page-footer bg-gray-base context-dark novi-background" data-preset='{"title":"Footer","category":"footer","reload":false,"id":"footer"}'>
+            <div class="container">
+                <div class="row justify-content-md-center text-xl-left">
+                    <div class="col-md-12">
+                        <div class="row justify-content-sm-center">
+                            <p class="small text-darker offset-top-4">Copyright &copy; <span id="copyright-year"></span> Frozen Bacon</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+`;
     }
 }
 
-customElements.define('footer-component', Footer);
+customElements.define("footer-component", Footer);
--- public/components/header.js
+++ public/components/header.js
@@ -1,46 +1,95 @@
 ﻿class Header extends HTMLElement {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    connectedCallback() {
-        this.innerHTML = `
-      <style>
-        nav {
-          height: 40px;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          background-color:  #0a0a23;
-        }
+  connectedCallback() {
+    this.innerHTML = `
+   <header class="page-head" data-preset='{"title":"Header","category":"header","reload":true,"id":"header"}'>
+            <!-- RD Navbar Transparent-->
+            <div class="rd-navbar-wrap">
+                <nav class="rd-navbar rd-navbar-logo-center novi-background rd-navbar-light" data-md-device-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-static" data-lg-stick-up-offset="110px" data-lg-auto-height="true" data-md-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-stick-up="true">
+                    <div class="rd-navbar-inner">
+                        <div class="container">
+                            <div class="rd-navbar-top-block row justify-content-xl-center align-items-xl-center">
 
-        ul {
-          padding: 0;
-        }
-        
-        a {
-          font-weight: 700;
-          margin: 0 25px;
-          color: #fff;
-          text-decoration: none;
-        }
-        
-        a:hover {
-          padding-bottom: 5px;
-          box-shadow: inset 0 -2px 0 0 #fff;
-        }
-      </style>
-      <header>
-        <nav>
-          <ul>
-            <li><a href="about.html">About</a></li>
-            <li><a href="work.html">Work</a></li>
-            <li><a href="contact.html">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
-    `;
-    }
+                                <div class="col-xl-12 text-center">
+                                    <!--Navbar Brand-->
+                                    <div class="rd-navbar-brand"><a href="/index.html"><img class="frozenbacon-logo" src='../images/logo/frozenbacon_logo_2.png' alt='' /></a></div>
+                                </div>
+                            </div>
+                            <div class="rd-navbar-panel">
+                                <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar, .rd-navbar-nav-wrap"><span></span></button>
+                            </div>
+                        </div>
+                        <div class="rd-navbar-menu-wrap">
+                            <div class="container">
+                                <div class="rd-navbar-nav-wrap">
+                                    <div class="rd-navbar-mobile-scroll">
+                                        <!--Navbar Brand Mobile-->
+                                        <div class="rd-navbar-mobile-brand"><a href="../index.html"><img class="frozenbacon-logo" src='../images/logo/frozenbacon_logo_2.png' alt='' /></a></div>
+                                        <!-- RD Navbar Nav-->
+                                        <ul class="rd-navbar-nav">
+                                            <li>
+                                                <a href="/index.html"><span>Home</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="#"><span>New</span></a>
+                                            </li>
+                                            <li class="active">
+                                                <a href="/gallery.html"><span>Art Galleries</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="#"><span>Landscape</span></a>
+                                                <ul class="rd-navbar-dropdown">
+                                                    <li>
+                                                        <a href="#"><span class="text-middle">Ocean</span></a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#"><span class="text-middle">Mountains</span></a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                            <li>
+                                                <a href="#"><span>Serverless Functions</span></a>
+                                                <ul class="rd-navbar-dropdown">
+                                                    <li>
+                                                        <a href="/test1.html"><span class="text-middle">Test 1</span></a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="/test2.html"><span class="text-middle">Test 2</span></a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <section>
+            <!-- Modern Breadcrumbs-->
+            <div class="parallax-container breadcrumb-modern bg-gray-darkest context-dark" data-parallax-img="images/home04.png">
+                <div class="parallax-content">
+                    <div class="bg-overlay-gray-darkest">
+                        <div class="container section-34 section-md-50 section-lg-110">
+                            <div class="text-extra-big font-accent d-none d-lg-block"><span class="big">Art Galleries</span></div>
+                            <ul class="list-inline list-inline-dashed p offset-top-0">
+                                <li class="list-inline-item"><a class="small" href="index.html">Home</a></li>
+                                <li class="list-inline-item">
+                                    Art Galleries
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+`;
+  }
 }
 
-customElements.define('header-component', Header);
+customElements.define("header-component", Header);
